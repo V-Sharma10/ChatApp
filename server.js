fa319cd640f72dcb6469b6696d670f17b34cddfd
Vivek Sharma <vivek.dakshana17@gmail.com>
2019-09-18 19:45:56 +0530

    node & socket.io

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3010;
+const PORT = process.env.PORT || 5000;
 const bodyPaser = require('body-parser');
 // const cors = require('cors');
 // app.use(cors);
